122e98a3d4f39a22df52fc2d09144d9bc306a2f2
renedietzjs <kuchenmann.rene@gmail.com>
2023-09-18 20:01:47 +0200

    style: renewed education section

--- src/components/organisms/OEducation.vue
+++ src/components/organisms/OEducation.vue
@@ -1,85 +1,124 @@
+<!-- eslint-disable vue/no-v-html -->
 <template>
-  <MBoxWithTitle :title="title">
-    <AColumns
-      v-for="(
-        { period, location, degree, gpa, university, activities }, i
-      ) of education"
-      :key="degree"
-      :class="['is-gapless', { 'mb-5': i !== education.length - 1 }]"
-    >
-      <AColumn
-        :class="[
-          'is-3',
-          isMobileScreen ? 'has-text-centered' : 'has-text-right',
-        ]"
-      >
-        <ASubtitle :text="period" class="mb-0" />
-        <p>{{ location }}</p>
-      </AColumn>
-      <AColumn class="is-narrow m-4" />
+  <MBoxWithTag title="My education">
+    <AColumns>
       <AColumn class="is-6">
-        <AExtendBg :top="3.25 + 0.5">
-          <p>
-            <ASubtitle
-              tag="span"
-              :text="degree"
-              class="has-text-weight-semibold mr-2"
-            />
-            <span class="has-text-grey">{{ gpa }}</span>
-          </p>
-          <p>{{ university }}</p>
-        </AExtendBg>
-        <ASpace value="2" />
-        <AHeading
-          v-for="({ property, description, linkage }, j) of activities"
-          :key="property"
-          :text="property"
-          :class="{ 'mb-3': j !== activities.length - 1 }"
+        <OCarousel
+          v-model="carouselItemNumber"
+          class="h-full"
+          :interval="15000"
+          :arrow="false"
+          repeat
+          autoplay
+          indicator-inside
         >
-          <MTextWithLink
-            v-if="linkage"
-            tag="span"
-            :text="description"
-            :linkage="linkage"
-          />
-          <span v-else>{{ description }}</span>
-        </AHeading>
+          <OCarouselItem
+            v-for="{ topic } of educationStore.$state"
+            :key="topic"
+          >
+            <section
+              :class="[
+                'box is-flex is-align-items-center is-justify-content-center h-full py-6',
+                `has-background-${isDark ? 'dark' : 'white'}`,
+              ]"
+            >
+              <AIcon
+                icon="school"
+                size="large"
+                :variant="isDark ? 'secondary' : 'dark'"
+              />
+              <p class="title ml-3">{{ topic }}</p>
+            </section>
+          </OCarouselItem>
+        </OCarousel>
+      </AColumn>
+      <AColumn class="is-narrow"></AColumn>
+      <AColumn>
+        <div class="block">
+          <p class="heading">{{ getEducationItem('period') }}</p>
+          <p>
+            <span class="title is-5 mr-2">{{
+              getEducationItem('degree')
+            }}</span>
+            <span class="has-text-grey">{{ getEducationItem('gpa') }}</span>
+          </p>
+          <p>
+            <span class="mr-2">{{ getEducationItem('university') }}</span>
+            <span class="has-text-grey">{{
+              getEducationItem('location')
+            }}</span>
+          </p>
+        </div>
+        <ul>
+          <li
+            v-for="({ name, descriptionInMd }, i) of getEducationItem(
+              'activities',
+            ) as {
+              name: string;
+              descriptionInMd: string;
+            }[]"
+            :key="name"
+            :class="{ 'mb-3': i !== getEducationItem('activities').length - 1 }"
+          >
+            <span class="has-text-weight-medium mr-2">{{ name }}</span>
+            <span
+              v-html="educationStore.descriptionInHtml(descriptionInMd).value"
+            ></span>
+          </li>
+        </ul>
       </AColumn>
     </AColumns>
-  </MBoxWithTitle>
+  </MBoxWithTag>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import MBoxWithTitle from '@molecules/MBoxWithTitle.vue';
-import MTextWithLink from '@molecules/MTextWithLink.vue';
+import MBoxWithTag from '@molecules/MBoxWithTag.vue';
 import AColumns from '@atoms/AColumns.vue';
 import AColumn from '@atoms/AColumn.vue';
-import ASubtitle from '@atoms/ASubtitle.vue';
-import AExtendBg from '@atoms/AExtendBg.vue';
-import ASpace from '@atoms/ASpace.vue';
-import AHeading from '@atoms/AHeading.vue';
-import { useEducationStore, useMobileBreakpoint, store } from '@stores';
+import AIcon from '@atoms/AIcon.vue';
+import { OCarousel, OCarouselItem } from '@oruga-ui/oruga-next';
+import { useEducationStore, useDarkMode, store, type Education } from '@stores';
 
 export default defineComponent({
   name: 'OEducation',
   components: {
-    MBoxWithTitle,
+    MBoxWithTag,
     AColumns,
     AColumn,
-    ASubtitle,
-    AExtendBg,
-    ASpace,
-    AHeading,
-    MTextWithLink,
+    OCarousel,
+    OCarouselItem,
+    AIcon,
   },
   setup() {
-    const { title, education } = useEducationStore(store);
-    const isMobileScreen = useMobileBreakpoint();
+    const educationStore = useEducationStore(store);
+    const isDark = useDarkMode();
 
-    return { title, education, isMobileScreen };
+    return { educationStore, isDark };
+  },
+  data() {
+    return {
+      carouselItemNumber: 0,
+    };
+  },
+  methods: {
+    getEducationItem(key: keyof Education) {
+      return this.educationStore[this.carouselItemNumber]![key];
+    },
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+:deep(.carousel .carousel-scene) {
+  height: 100%;
+}
+
+:deep(.carousel .carousel-scene .carousel-items) {
+  height: 100%;
+}
+
+:deep(span > p) {
+  display: inline !important;
+}
+</style>
--- src/stores/education.ts
+++ src/stores/education.ts
@@ -1,73 +1,76 @@
 import { defineStore } from 'pinia';
-import type { Linkage } from '.';
+import { useMarkdownToHtml } from '.';
 
 export const useEducationStore = defineStore('education', {
-  state: () => ({
-    title: 'Education',
-    education: [
-      {
-        period: '2020 – 2022',
-        location: 'Cologne, Germany',
-        degree: 'M.Sc. Media Technology',
-        gpa: 'Ø1.2',
-        university: 'TH Köln – University of Applied Sciences',
-        activities: [
-          {
-            property: 'Achievement',
-            description: `Participation in the first round of the entrepreneurship program KickStart@TH Köln in 2021 to fund the master's project PhoGrow`,
-            linkage: {
-              title: 'KickStart@TH Köln',
-              href: 'https://www.th-koeln.de/forschung/kickstartth-koeln_77116.php',
-            },
-          },
-          {
-            property: 'Thesis title',
-            description:
-              'A survey on NFT development with EVM-compatible DLT/L2 and use cases for NFTs in the digital and real world',
-          },
-          {
-            property: 'Main areas of study',
-            description:
-              'Interactive media applications, audiovisual media technologies and systems, and imaging technology',
-          },
-        ],
-      },
-      {
-        period: '2015 – 2020',
-        location: 'Cologne, Germany',
-        degree: 'B.Sc. Media Technology',
-        gpa: 'Ø1.9',
-        university: 'TH Köln – University of Applied Sciences',
-        activities: [
-          {
-            property: 'Thesis title',
-            description:
-              'Implementation of an arpeggiator through a web application – functionality and user interface',
-          },
-          {
-            property: 'Main areas of study',
-            description:
-              'Web engineering, media transmission/display technology, production technology for audiovisual media, and image processing',
-          },
-          {
-            property: 'Internship ',
-            description: `Technical acoustician at IST Ingenieurbüro Stöcker (Burscheid, Germany) from 10/2019 – 12/2019`,
-          },
-        ],
-      },
-    ] as Education[],
-  }),
+  state: (): Education[] => [
+    {
+      topic: 'Master studies',
+      period: '2020 – 2022',
+      location: 'Cologne, Germany',
+      degree: 'M.Sc. Media Technology',
+      gpa: 'Ø1.2',
+      university: 'TH Köln – University of Applied Sciences',
+      activities: [
+        {
+          name: 'Achievement',
+          descriptionInMd: `Participation in the first round of the entrepreneurship program [KickStart@TH Köln](https://www.th-koeln.de/forschung/kickstartth-koeln_77116.php) in 2021 to fund the master's project PhoGrow`,
+        },
+        {
+          name: 'Thesis title',
+          descriptionInMd:
+            'A survey on NFT development with EVM-compatible DLT/L2 and use cases for NFTs in the digital and real world',
+        },
+        {
+          name: 'Main areas of study',
+          descriptionInMd:
+            'Interactive media applications, audiovisual media technologies and systems, and imaging technology',
+        },
+      ],
+    },
+    {
+      topic: 'Bachelor studies',
+      period: '2015 – 2020',
+      location: 'Cologne, Germany',
+      degree: 'B.Sc. Media Technology',
+      gpa: 'Ø1.9',
+      university: 'TH Köln – University of Applied Sciences',
+      activities: [
+        {
+          name: 'Thesis title',
+          descriptionInMd:
+            'Implementation of an arpeggiator through a web application – functionality and user interface',
+        },
+        {
+          name: 'Main areas of study',
+          descriptionInMd:
+            'Web engineering, media transmission/display technology, production technology for audiovisual media, and image processing',
+        },
+        {
+          name: 'Internship',
+          descriptionInMd: `Technical acoustician at IST Ingenieurbüro Stöcker (Burscheid, Germany) from 10/2019 – 12/2019`,
+        },
+      ],
+    },
+  ],
+  getters: {
+    descriptionInHtml: () => {
+      return (descriptionInMd: string) => {
+        const { html } = useMarkdownToHtml(descriptionInMd);
+        return html;
+      };
+    },
+  },
 });
 
 export interface Education {
+  topic: string;
   period: string;
   location: string;
   degree: string;
   gpa: string;
   university: string;
   activities: {
-    property: string;
-    description: string;
-    linkage?: Linkage;
+    name: string;
+    descriptionInMd: string;
   }[];
 }
